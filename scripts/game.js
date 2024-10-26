48b5416a4b91fb9fd20feac1bdc4c6b8f1673e10
dananasr2004 <dananasr871@gmail.com>
2024-10-26 23:09:12 +0200

    Add:dashes and correct letters

--- scripts/game.js
+++ scripts/game.js
@@ -11,21 +11,42 @@ var word;
 var answer = document.getElementById("answer-section");
 var start = document.getElementById("start-button");
 var pressedLetter = document.querySelectorAll(".letter");
+var guessed_letters = "";
+var dash;
+var flag=0;
+window.addEventListener("keydown", checkIfEnter);
 
-
-
-start.addEventListener("click", startGame);
-
+function checkIfEnter(event) {
+  if (event.key == "Enter") {
+    startGame();
+  }
+}
 function startGame() {
   word = words[Math.floor(Math.random() * words.length)];
   drawDashes();
 }
 
 function drawDashes() {
-  answer.textContent = word
+  dash = answer.textContent = word
     .split("")
-    .map((letter) => "_")
+    .map(() => "_")
     .join(" ");
+
+  document.querySelectorAll(".letter").forEach((letter) => {
+    letter.addEventListener("click", () => {
+      checkCorrectLetters(letter.textContent);
+    });
+  });
 }
-
-
+function checkCorrectLetters(gussed_letter) {
+  var n_dash = dash.split(" ");
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === gussed_letter ) {
+      n_dash[i] = word[i];
+      flag=1;
+      console.log(n_dash);
+    } 
+  }
+  dash = n_dash.join(" ");
+  answer.textContent = dash;
+}
