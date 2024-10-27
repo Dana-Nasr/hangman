b022bdea3054429bab360a0d32d66ff4ecb36c9c
dananasr2004 <dananasr871@gmail.com>
2024-10-27 14:11:06 +0200

    comment: commenting code

--- scripts/game.js
+++ scripts/game.js
@@ -16,26 +16,26 @@ var hang = 0;
 var textNode = document.createTextNode("Press enter to start");
 document.querySelector(".hangman").appendChild(textNode);
 
-window.addEventListener("keydown", checkIfEnter);
+window.addEventListener("keydown", checkIfEnter);  //listen key board check if key enter
 
 function checkIfEnter(event) {
   if (event.key == "Enter") {
-    document.querySelector(".hangman").removeChild(textNode);
+    document.querySelector(".hangman").removeChild(textNode); //remove to delete press enter to strt
     startGame();
   }
 }
 function startGame() {
-  word = words[Math.floor(Math.random() * words.length)];
+  word = words[Math.floor(Math.random() * words.length)]; //radom choice of word from the array
   drawDashes();
 }
 
-function drawDashes() {
+function drawDashes() { //display dashes on screen split every letter alone and represent by dash
   dash = answer.textContent = word
     .split("")
     .map(() => "_")
     .join(" ");
 
-  document.querySelectorAll(".letter").forEach((letter) => {
+  document.querySelectorAll(".letter").forEach((letter) => {   //when the user press a letter removes it and check if correct 
     letter.addEventListener("click", () => {
       letter.style.display = "none";
       checkCorrectLetters(letter.textContent);
@@ -45,10 +45,10 @@ function drawDashes() {
 
 function checkCorrectLetters(gussed_letter) {
   var n_dash = dash.split(" ");
-  for (let i = 0; i < word.length; i++) {
+  for (let i = 0; i < word.length; i++) { //traversal of word str and check with guessed letter o(n)
     if (word[i] === gussed_letter) {
-      n_dash[i] = word[i];
-      flag = 1;
+      n_dash[i] = word[i];      //create new lst to save letters 
+      flag = 1;                 //flag to detect letter is found else add part to hang
       console.log(n_dash);
     }
   }
@@ -57,6 +57,6 @@ function checkCorrectLetters(gussed_letter) {
     addParts(hang);
   }
   flag = 0;
-  dash = n_dash.join(" ");
+  dash = n_dash.join(" "); //change list to str and display
   answer.textContent = dash;
 }
--- scripts/hang.js
+++ scripts/hang.js
@@ -8,7 +8,7 @@ function createParts(source, css_class) {
 }
 
 
-function addParts(hang) {
+function addParts(hang) {           //ading parts to the hangman whn letter guesed wrong o(1)
   if (hang === 1) {
     part = createParts("./assets/head.svg", "head");
     hangman.appendChild(part);
